71cc5f668b65ba28634d175e1809a4c847f8a4d9
LinuxCat <masch77.linuxcat@gmail.com>
2022-05-03 08:54:29 +0200

    change to py3, removed uselessness

--- roll.sh
+++ roll.sh
@@ -21,27 +21,10 @@ has?() { hash $1 2>/dev/null; }
 cleanup() { (( audpid > 1 )) && kill $audpid 2>/dev/null; }
 quit() { echo -e "\x1b[2J \x1b[0H ${purp}<3 \x1b[?25h \x1b[u \x1b[m"; }
 
-usage () {
-  echo -en "${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT."
-  echo -e "  ${purp}[v$version]"
-  echo -e "${yell}Usage: ./astley.sh [OPTIONS...]"
-  echo -e "${purp}OPTIONS : ${yell}"
-  echo -e " help   - Show this message."
-  echo -e " inject - Append to ${purp}${USER}${yell}'s bashrc. (Recommended :D)"
-}
 for arg in "$@"; do
-  if [[ "$arg" == "help"* || "$arg" == "-h"* || "$arg" == "--h"* ]]; then
-    usage && exit
-  elif [[ "$arg" == "inject" ]]; then
-    echo -en "${red}[Inject] "
+  if [[ "$arg" == "inject" ]]; then
     echo $NEVER_GONNA >> $MAKE_YOU_CRY
-    echo -e "${green}Appended to $MAKE_YOU_CRY. <3"
-    echo -en "${yell}If you've astley overdosed, "
-    echo -e "delete the line ${purp}\"$NEVER_GONNA\"${yell}."
     exit
-  else
-    echo -e "${red}Unrecognized option: \"$arg\""
-    usage && exit
   fi
 done
 trap "cleanup" INT
@@ -74,7 +57,7 @@ audpid=$!
 #echo -e "${yell}Fetching video..."
 # Sync FPS to reality as best as possible. Mac's freebsd version of date cannot
 # has nanoseconds so inject python. :/
-python <(cat <<EOF
+python3 <(cat <<EOF
 import sys
 import time
 fps = 25; time_per_frame = 1.0 / fps
